abd04a57619ca841f6f1501ab9a66a8511e0f21b
sebek92 <sebastian.semczyszyn@gmail.com>
2015-01-22 10:50:10 +0100

    dodaje pacjenta

---
+++ Przychodnia/src/przychodnia/Pacjent.java
@@ -0,0 +1,19 @@
+package przychodnia;
+
+/**
+ * Klasa ma służyć to reprezentowania pacjenta
+ * @author Sebastian
+ */
+public class Pacjent {
+    //pola
+    private String imie;
+    private String nazwisko;
+    private int pesel;
+    
+    public Pacjent(String imie, String nazwisko, int pesel){
+        
+    }
+    //public Pacjent
+    
+    
+}
