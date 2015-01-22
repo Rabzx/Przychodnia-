269b74bfd23860fb5fa0039245db29bb44a4926e
Radosław Czerwieniec <radoslaw.czerwieniec@gmail.com>
2015-01-22 11:04:13 +0100

    Dodaje klase Administracja

---
+++ Przychodnia/src/przychodnia/Administracja.java
@@ -0,0 +1,25 @@
+package przychodnia;
+
+/**
+ * Klasa przedstawiajaca Administracje
+ * @author Radosław
+ */
+public class Administracja {
+    
+    // pola
+    private String NazwaDzialu;
+    private int IDpracownika;
+    private int IDczynnosci;
+    
+    public Administracja(String NazwaDzialu, int IDpracownika) //konstruktor
+    {
+        
+    }
+    
+    int DodanieCzynności() // Dodaje zadanie do Pracownika
+    {
+        
+        return IDczynnosci;
+    }
+    
+}
