23e58bc8359af0a32aa42279c14a59e416819822
sebek92 <sebastian.semczyszyn@gmail.com>
2015-01-22 11:27:14 +0100

    dodaje klase zabiegi

---
+++ Przychodnia/src/przychodnia/Zabiegi.java
@@ -0,0 +1,28 @@
+
+package przychodnia;
+
+/**
+ * Klasa reprezentująca zabiegi
+ * @author student
+ */
+public class Zabiegi {
+    //pola
+   private int IdZabiegu;
+   private String OpisZabiegu;
+   private int data;
+   
+   public Zabiegi(){
+       
+   }//public Zabiegi
+   
+   //ustala kolejkę do zabiegu
+   int dodajZabieg()
+   {
+       return IdZabiegu;
+   }//int dodajZabieg
+   
+   //metoda ustala date zabiegu
+   int dodajDate(){
+       return data;
+   }//int dodaDate
+}
