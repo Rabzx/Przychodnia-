2eb729c9a4ecc3767ef52c210da0eaab589da6c7
Radosław Czerwieniec <radoslaw.czerwieniec@gmail.com>
2015-01-22 11:28:40 +0100

    dodaje choroby

---
+++ Przychodnia/src/przychodnia/Choroby.java
@@ -0,0 +1,21 @@
+package przychodnia;
+
+/**
+ *Klasa opisujaca choroby
+ * @author Radoslaw
+ */
+public class Choroby {
+    
+    private String NazwaChoroby;
+    private int StopienZagrozenia;
+    
+    public Choroby()
+    {
+        
+    }
+    
+    public Choroby(String Nazwa, int Stopien)
+    {
+        
+    }
+}
